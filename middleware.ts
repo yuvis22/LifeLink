2136459d58bddd69884f3bebd3b8b16c4eb43a4c
Yuvraj Singh <e21cseu0081@bennett.edu.in>
2025-04-29 15:59:14 +0530

    Test deploy

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const handler = clerkMiddleware();
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-};
+export default function middleware(request: NextRequest, event: NextFetchEvent) {
+  try {
+    return handler(request, event);
+  } catch (error) {
+    console.error("Middleware error:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
